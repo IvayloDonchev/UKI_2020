6d56ab1a1a3f51a497b9392fbed7df5f0ea9020c
Ivaylo Donchev <idonchev@hotmail.com>
2020-02-28 13:53:15 +0200

    second

--- Guess the Number/Guess the Number/MainWindow.xaml.cs
+++ Guess the Number/Guess the Number/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Guess_the_Number
 {
@@ -61,6 +49,11 @@ namespace Guess_the_Number
                 BtnTry.IsEnabled = false;
                 BtnStart.IsEnabled = true;
             }
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
 
         public MainWindow()
