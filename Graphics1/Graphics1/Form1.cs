1bf4fb51f35a841cd6ab5a8cd7f3a36551c47341
Ivaylo Donchev <idonchev@hotmail.com>
2020-04-27 14:01:57 +0300

    ytt

---
+++ Graphics1/Graphics1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphics1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.Black, 2);
+            int side = 200, x1 = 100, y1 = 300, x2, y2, x3, y3;
+            x2 = x1 + side; y2 = y1; x3 = (x1 + x2) / 2;
+            int h = (int)(side * Math.Sqrt(3) / 2);
+            y3 = y1 - h;
+            for (int i = 1; i <= 100; i+=5)
+            {
+                g.DrawLine(p, x1+i, y1+i, x2+i, y2+i);
+                g.DrawLine(p, x2+i, y2+i, x3+i, y3+i);
+                g.DrawLine(p, x3+i, y3+i, x1+i, y1+i);
+            }
+            
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Graphics2/Graphics2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Graphics2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Add a Line Element
+            PointCollection myPointCollection = new PointCollection();
+            myPointCollection.Add(new Point(0, 0));
+            myPointCollection.Add(new Point(0, 1));
+            myPointCollection.Add(new Point(1, 1));
+
+            Polygon myPolygon = new Polygon();
+            myPolygon.Points = myPointCollection;
+            myPolygon.Fill = Brushes.Blue;
+            myPolygon.Width = 100;
+            myPolygon.Height = 100;
+            myPolygon.Stretch = Stretch.Fill;
+            myPolygon.Stroke = Brushes.Black;
+            myPolygon.StrokeThickness = 2;
+            myGrid.Children.Add(myPolygon);
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
